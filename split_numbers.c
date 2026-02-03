fab3f7ed1f8c75ec05fe7beca2fbab0097dd52dc
Auxence Sauvaget <asauvage@f2r10s18.clusters.42paris.fr>
2026-02-03 17:00:55 +0100

    boumboum

--- split_numbers.c
+++ split_numbers.c
@@ -12,33 +12,30 @@
 
 #include "push_swap.h"
 
+void	verif_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (ft_isspace(str[i]))
+			i++;
+		if (str[i] == '-' || str[i] == '+')
+			i++;
+		while (ft_isdigit(str[i]))
+			i++;
+	}
+}
+
 void	verif_numbers(char **nbs)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (nbs && nbs[i])
 	{
-		j = 0;
-		while (nbs[i][j])
-		{
-			while (ft_isspace(nbs[i][j]))
-				j++;
-			if (nbs[i][j] == '-' || nbs[i][j] == '+')
-				j++;
-			while (ft_isdigit(nbs[i][j]))
-				j++;
-			if (!ft_isspace(nbs[i][j]))
-				break ;
-		}
-		ft_printf("%d, %d\n", ft_strlen(nbs[i]), j);
-		if (ft_strlen(nbs[i]) != (size_t)j)
-		{
-			ft_printf("Error list numbers\n");
-			exit(1);
-		}
-		i++;
+		verif_str(nbs[i]);
 	}
 }
 
